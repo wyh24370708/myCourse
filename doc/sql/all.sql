8fc8c433fdb4f4765adf536e9f3cab6efd5c4358
wangyonghao <wyh24370708@163.com>
2020-05-15 18:12:39 +0800

    1.idea整合数据源 2.配置热部署 3.配置控制台打印日志 再次提交

---
+++ doc/sql/all.sql
@@ -0,0 +1,11 @@
+drop table if exists course;
+CREATE TABLE `course` (
+    `id` varchar(52) NOT NULL COMMENT '编号',
+    `name` varchar(52) NOT NULL COMMENT '姓名',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+insert into course values('1','李正龙');
+insert into course values('2','王勇豪');
+insert into course values('3','张杰');
