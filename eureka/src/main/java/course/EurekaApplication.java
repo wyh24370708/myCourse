88183fe18f4c8cd38905b0c6ac0ec09e9f88929e
wangyonghao <wyh24370708@163.com>
2020-05-15 13:52:35 +0800

    1. 美化eureka模块启动日志

--- eureka/src/main/java/course/EurekaApplication.java
+++ eureka/src/main/java/course/EurekaApplication.java
@@ -1,14 +1,25 @@
 package course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		//原来
+//		SpringApplication.run(EurekaApplication.class, args);
+		//美化后的启动日志
+		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		ConfigurableEnvironment env = app.run(args).getEnvironment();
+		LOG.info("eureka启动了! ! !");
+		LOG.info("eureka地址: \thttp:127.0.0.1:{}",env.getProperty("server.port"));
 	}
 }
