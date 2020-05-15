7bbab77800a838dfd417cff8b7ea9e020d26a5c3
wangyonghao <wyh24370708@163.com>
2020-05-15 13:14:31 +0800

    1. 新增gateway模块 2. 增加gateway依赖 3. 注册到eureka注册中心

---
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.course.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        LOG.info("gateway模块 启动了! ! !");
+        LOG.info("gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+}
