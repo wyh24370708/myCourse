714b3b0390157afe7e98169a006eb8f2c79cfc40
wangyonghao <wyh24370708@163.com>
2020-05-16 11:25:26 +0800

    1. 集成bootStrap模板ace

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,11 @@
+//本地为ES5,vue为ES6,报错转换一下
+//main.js是vue的入口文件n,初始化vue实例,集成所需要的插件
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//渲染id为app的样式
 new Vue({
   render: h => h(App),
 }).$mount('#app')
