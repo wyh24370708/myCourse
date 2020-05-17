267b20d2259eb0b2aa2cc09f8d1123b2f98b959c
wangyonghao <wyh24370708@163.com>
2020-05-17 16:56:40 +0800

    1. 新建business模块(其他文件) 2. 创建chapter表,

--- doc/sql/all.sql
+++ doc/sql/all.sql
@@ -1,3 +1,15 @@
+-- 大章
+drop table if exists `chapter`;
+create table `chapter` (
+        `id` int not null auto_increment comment '编号',
+        `course_id` varchar(8) unique comment '课程id',
+        `name` varchar(52) not null comment '名称',
+        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment '大章';
+
+
+
+-- 测试
 drop table if exists course;
 CREATE TABLE `course` (
     `id` varchar(52) NOT NULL COMMENT '编号',
