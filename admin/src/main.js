8c55384021c818053b168d3c4a287abdcf58b1fe
wangyonghao <wyh24370708@163.com>
2020-05-18 13:05:17 +0800

    1. main.js引入axios完成前后端交互

--- admin/src/main.js
+++ admin/src/main.js
@@ -4,8 +4,12 @@ import Vue from 'vue'
 import App from './App.vue'
 //引入router组件
 import router from './router'
+//引入axios
+import Axios from "axios";//Alt+Enter加入字典中
 
 Vue.config.productionTip = false
+//理解为全局变量,在任意vue组件使用,this.$ajax获取值  $表示Vue全局属性的一个约定
+Vue.prototype.$ajax = Axios;
 
 //渲染id为app的样式
 new Vue({
