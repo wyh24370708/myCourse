50d01dabb1ecc65a8092877e69ec32abbd9d3946
wangyonghao <wyh24370708@163.com>
2020-05-18 14:14:31 +0800

    1. gataway中配置过滤器解决跨域问题 2. 使用lb://注册中心名字,来做路由转发

--- gateway/src/main/java/com/course/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -5,7 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,4 +23,28 @@ public class GatewayApplication {
         LOG.info("gateway模块 启动了! ! !");
         LOG.info("gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
+
+    /**
+     * gateway中解决跨域问题
+     * 过滤器
+     * @return
+     */
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowCredentials(Boolean.TRUE);// 允许cookies跨域
+        corsConfig.addAllowedHeader("*");// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+        corsConfig.addAllowedMethod("*");// 允许提交请求的方法类型，*表示全部允许
+        corsConfig.addAllowedOrigin("*");// #允许访问的头信息,*表示全部
+        corsConfig.setMaxAge(3600L);// 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        CorsWebFilter corsWebFilter = new CorsWebFilter(source);
+        return corsWebFilter;
+    }
+
+
 }
--- server/src/main/java/com/course/server/config/CorsConfig.java
+++ server/src/main/java/com/course/server/config/CorsConfig.java
@@ -1,23 +1,23 @@
-package com.course.server.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * 解决跨域问题
- */
-@Configuration//表明是配置类
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-                .allowCredentials(true)
-                .maxAge(3600);//一个小时内不会再校验(发OPTIONS请求)
-    }
-}
+//package com.course.server.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * 解决跨域问题
+// */
+//@Configuration//表明是配置类
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders(CorsConfiguration.ALL)
+//                .allowedMethods(CorsConfiguration.ALL)
+//                .allowCredentials(true)
+//                .maxAge(3600);//一个小时内不会再校验(发OPTIONS请求)
+//    }
+//}
