2ea3e61859d0aa6ab4a67c9583c942700311afa6
wangyonghao <wyh24370708@163.com>
2020-05-18 13:31:33 +0800

    1. 使用gateway路由转发,vue页面只访问gateway

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -156,7 +156,7 @@
       methods: {
         list() {
             let _this = this;
-            _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then(function (response) {
+            _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then(function (response) {
                 //then异步执行,当.then()前的方法执行完后再执行then()内部的程序，这样就避免了，数据没获取到等的问题。
                 console.log("查询大章结果:",response);
                 // 出现跨域问题,因为我们是前后端分离的
