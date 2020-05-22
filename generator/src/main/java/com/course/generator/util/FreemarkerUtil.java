b6575106c14b972ec69a481a97e4d4669d40eb9c
wangyonghao <wyh24370708@163.com>
2020-05-22 13:03:26 +0800

    生成器开发: 1. 制作FreemarkerUtil,简化生成器的使用 2. 新建ServerGenerator模板生成器,用于生成后端代码: Controller service dto

---
+++ generator/src/main/java/com/course/generator/util/FreemarkerUtil.java
@@ -0,0 +1,45 @@
+package com.course.generator.util;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FreemarkerUtil {
+    /**
+     * ftlPath: 文件模板所在的文件夹路径
+     */
+    private static String ftlPath = "generator\\src\\main\\java\\com\\course\\generator\\ftl\\";
+    private static Template temp = null;
+
+    /**
+     * freemarker初始化
+     * @param fileName_from
+     * @throws IOException
+     */
+    public static void initConfig(String fileName_from) throws IOException {
+        Configuration config = new Configuration(Configuration.VERSION_2_3_29);
+        config.setDirectoryForTemplateLoading(new File(ftlPath));
+        config.setObjectWrapper(new DefaultObjectWrapper(Configuration.VERSION_2_3_29));
+        temp = config.getTemplate(fileName_from);
+    }
+
+    /**
+     * 生成模板文件
+     * @param fileName_to
+     * @throws IOException
+     * @throws TemplateException
+     */
+    public static void generator(String fileName_to) throws IOException, TemplateException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName_to));
+        temp.process(null,bw);
+        bw.flush();
+        bw.close();
+    }
+
+}
