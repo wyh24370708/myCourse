f7bd4003a81f7e2514c84d38e0b5f3796c355649
wangyonghao <wyh24370708@163.com>
2020-06-03 11:25:34 +0800

    文件管理功能: 1. 上传文件时保存文件记录

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -1,6 +1,9 @@
 package com.course.file.controller.admin;
 
+import com.course.server.dto.ProfileDto;
 import com.course.server.dto.ResponseDto;
+import com.course.server.enums.ProfileUseEnum;
+import com.course.server.service.ProfileService;
 import com.course.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 
@@ -19,6 +23,8 @@ public class UploadController {
     private static final Logger LOG = LoggerFactory.getLogger(UploadController.class);
     private static final String BUSINESS_NAME = "文件";
 
+    @Resource
+    private ProfileService profileService;
     //属性注入
     @Value("${file_up_path}")
     private String FILE_UP_PATH;
@@ -42,13 +48,23 @@ public class UploadController {
         String filename = file.getOriginalFilename();//文件名
         String uuid = UuidUtil.getShortUuid();//防止文件重复
         String fullPath = pathDir + uuid + "-" + filename;//目标路径
+        String profilePath = "teacher/" + uuid + "-" + filename;
         File dest = new File(fullPath);
         //上传到目标位置
         file.transferTo(dest);
         LOG.info(dest.getAbsolutePath());//本机中图片存储的路径
 
+        LOG.info("文件保存记录开始...");
+        String suffix = filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
+        ProfileDto profileDto = new ProfileDto();
+        profileDto.setPath(profilePath);
+        profileDto.setName(filename);
+        profileDto.setSize(Integer.parseInt(String.valueOf(file.getSize())));
+        profileDto.setSuffix(suffix);
+        profileDto.setUse(ProfileUseEnum.TEACHER.getCode());
+        profileService.save(profileDto);
         //配置静态资源之后, 路径对外暴露, 返回结果中存入访问地址 头像实时显示
-        String url = FILE_SERVER_PATH + "f/teacher/" + uuid + "-" + filename;
+        String url = FILE_SERVER_PATH + profilePath;
         responseDto.setContent(url);
         return responseDto;
     }
