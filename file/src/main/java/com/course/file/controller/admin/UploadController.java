c774690b9d3a622eea9c58d94b6e48497ee5e62d
wangyonghao <wyh24370708@163.com>
2020-06-05 10:32:15 +0800

    大文件断点续传: 1.按分片顺序合并成原始文件

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -6,7 +6,6 @@ import com.course.server.enums.ProfileUseEnum;
 import com.course.server.service.ProfileService;
 import com.course.server.util.Base64ToMultipartFile;
 import com.course.server.util.UuidUtil;
-import org.bouncycastle.jcajce.provider.digest.MD5;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +14,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/admin")
@@ -22,6 +23,7 @@ public class UploadController {
 
     private static final Logger LOG = LoggerFactory.getLogger(UploadController.class);
     private static final String BUSINESS_NAME = "文件";
+    private static Map<String,String> PATH_MAP = new HashMap<>();
 
     @Resource
     private ProfileService profileService;
@@ -57,7 +59,7 @@ public class UploadController {
         ProfileUseEnum useEnum = ProfileUseEnum.getEnumByCode(use);
         String  teacher = useEnum.name().toLowerCase();
         //指定上传的路径
-        String pathDir =  FILE_UP_PATH + teacher + File.separator;
+        String pathDir =  FILE_UP_PATH + teacher + "/";
         File fileDir = new File(pathDir);
         if (!fileDir.exists()){
             fileDir.mkdirs();//上传路径不存在就新创建路径
@@ -65,21 +67,69 @@ public class UploadController {
         // 保存文件到本地
 //        String fullPath = pathDir + key + "." + suffix;//目标路径
         String fullPath = new StringBuffer(pathDir).append(key)
+                .append(".").append(suffix).toString();
+
+        String shardPath = new StringBuffer(pathDir).append(key)
                 .append("_").append(profileDto.getShardIndex())
                 .append(".").append(suffix)
                 .toString();
-        String profilePath = teacher + File.separator + key + "." + suffix;
-        File dest = new File(fullPath);
+        PATH_MAP.put("PATH",fullPath);
+        PATH_MAP.put("PATH_"+profileDto.getShardIndex(),shardPath);
+        File dest = new File(shardPath);
         //上传到目标位置
         shard.transferTo(dest);
 
         LOG.info("文件保存记录开始...");
         //配置静态资源之后, 路径对外暴露, 返回结果中存入访问地址 头像实时显示
-        String url = FILE_SERVER_PATH + profilePath;
+        String url = new StringBuffer(FILE_SERVER_PATH).append(teacher).append("/").append(key)
+                .append(".").append(suffix).toString();
         profileDto.setPath(url);
         profileService.save(profileDto);
         responseDto.setContent(profileDto);
+
+        //校验并合并分片
+        if (profileDto.getShardIndex() == profileDto.getShardTotal()){
+            this.merge(profileDto);
+        }
+
+
         return responseDto;
+    }
+
+    /**
+     * 分片合并
+     */
+    public void merge(ProfileDto profileDto) throws IOException {
+        LOG.info("合并分片开始......");
+        File file = new File(PATH_MAP.get("PATH"));//文件输出的目标位置
+        FileOutputStream fos = new FileOutputStream(file, true);//true表示文件追加输出
+        FileInputStream fis = null;//声明输入流
+        byte[] bytes = new byte[10 * 1024 * 1024];//每次读写的字节数
+        int len = 0;
+        try {
+            //循环合并
+            for (int i = 1, LEN = profileDto.getShardTotal(); i <= LEN; i++){
+                fis = new FileInputStream(new File(PATH_MAP.get("PATH_"+i)));//分片
+                while ((len = fis.read(bytes))!= -1){
+                    fos.write(bytes,0, len);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOG.error("分片合并异常", e);
+        }finally {
+            try {
+                if (fis != null){
+                    fos.close();
+                    fis.close();
+                }
+                LOG.info("IO流关闭");
+            } catch (IOException e) {
+                e.printStackTrace();
+                LOG.error("IO流关闭错误");
+            }
+        }
+        LOG.info("合并分片结束......");
     }
 
     /**
