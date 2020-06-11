dcb08eee380e342e7e2ac01180464702e4b9b18e
wangyonghao <wyh24370708@163.com>
2020-06-11 10:40:16 +0800

    用户登录: 1.拦截接口:gateway实现控台登录拦截功能 2.BUG: 登录后控台页面会时不时未授权,数据无法显示

--- gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilter.java
+++ gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilter.java
@@ -5,21 +5,58 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
 
 @Component
 public class LoginAdminGatewayFilter implements GatewayFilter, Ordered {
 
     private static final Logger LOG = LoggerFactory.getLogger(LoginAdminGatewayFilter.class);
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        //获取header的token参数
+        String path = exchange.getRequest().getURI().getPath();
+
+        // 请求地址中不包含/admin/的，不是控台请求，不需要拦截
+        if (!path.contains("/admin")){
+            LOG.info("非admin控台登录验证：{}", path);
+            return chain.filter(exchange);
+        }
+        //登陆,退出,验证码不拦截
+        if (path.contains("/system/admin/user/login")
+            || path.contains("/system/admin/user/logout")
+            || path.contains("/system/admin/imageCode/checkCode/")){
+
+            LOG.info("不需要控台登录验证：{}", path);
+            return chain.filter(exchange);
+        }
+
+        //获取header的token参数   token为null拦截
         String token = exchange.getRequest().getHeaders().getFirst("token");
-        LOG.info("登陆拦截开始:token={}",token);
-        return chain.filter(exchange);
+        LOG.info("控台登陆验证开始:token={}",token);
+        if(StringUtils.isEmpty(token) || token == null){
+            LOG.info("token=null,请求被拦截");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);//401未授权
+            return exchange.getResponse().setComplete();
+        }
+        Object loginUser = redisTemplate.opsForValue().get(token);
+        if (loginUser == null){//拦截
+            LOG.info("token无效,请求被拦截");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }else{//放行
+            LOG.info("登陆成功:{}",loginUser);
+            return chain.filter(exchange);
+        }
     }
 
     @Override
