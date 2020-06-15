c1b1d12ca2e94221103a67cce04c0f2c6c1916bd
wangyonghao <wyh24370708@163.com>
2020-06-15 16:04:14 +0800

    网站开发: 1.首页开发，将底部提取成myFooter组件

--- web/src/app.vue
+++ web/src/app.vue
@@ -162,23 +162,15 @@
 
     </main>
 
-    <footer class="text-muted">
-      <div class="container">
-        <p class="float-right">
-          <a href="#">Back to top</a>
-        </p>
-        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-        <p>New to Bootstrap? <a href="https://v4.bootcss.com/">Visit the homepage</a> or read our <a href="/docs/getting-started/introduction/">getting started guide</a>.</p>
-      </div>
-    </footer>
-
+    <my-footer></my-footer>
   </div>
 </template>
 
 <script>
   import MyHeader from "./components/myHeader";
+  import MyFooter from "./components/myFooter";
   export default {
     name: 'app',
-    components: {MyHeader},
+    components: {MyFooter, MyHeader},
   }
 </script>
