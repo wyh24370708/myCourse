90111cb22a250410d3cc49a69eb2cd9e7ade7f5a
wangyonghao <wyh24370708@163.com>
2020-06-17 15:58:08 +0800

    网站开发: 1. 课程详情页面开发，增加课程信息显示    可以使用@media（最大宽度：700像素）来精细控制不同分辨率的显示样式

--- web/src/views/detail.vue
+++ web/src/views/detail.vue
@@ -3,7 +3,31 @@
     <main role="main">
       <div class="album py-5 bg-light">
         <div class="container">
-         <h3>课程详情</h3>
+          <div class="row course-head">
+            <!--课程封面 start-->
+            <div class="col-sm-6" id="cover-video-div">
+              <img class="img-fluid" v-bind:src="course.image">
+            </div>
+            <!--课程封面 end-->
+
+            <!--详情 start-->
+            <div class="col-sm-6">
+              <h1>{{course.name}}</h1>
+              <p class="course-head-item">
+                <span><i class="fa fa-clock-o"></i> {{(course.time) | formatSecond}}</span>
+                <span>{{COURSE_LEVEL | optionKV(course.level)}}</span>
+                <span><i class="fa fa-user"></i> {{course.enroll}}</span>
+              </p>
+              <p class="course-head-desc">{{course.summary}}</p>
+              <p class="course-head-price">
+                <span class="price-now text-danger"><i class="fa fa-yen"></i>&nbsp;{{course.price}}&nbsp;&nbsp;</span>
+              </p>
+              <p class="course-head-button-links">
+                <a class="btn btn-lg btn-primary btn-shadow" href="javascript:;">立即报名</a>
+              </p>
+            </div>
+            <!--详情 end-->
+          </div>
         </div>
       </div>
     </main>
@@ -14,17 +38,67 @@
   export default {
     name: "detail",
     data:function () {
-      return{}
+      return{
+        id: "",
+        course:{},
+        teacher:{},
+        chapters:[],
+        sections:[],
+        COURSE_LEVEL: COURSE_LEVEL
+      }
     },
     mounted() {
-
+      let _this = this;
+      _this.id = _this.$route.query.id;
+      _this.findCourse();
     },
     methods:{
-
+      /**
+       * 课程详情
+       */
+      findCourse(){
+        let _this = this;
+        _this.$ajax.get(process.env.VUE_APP_SERVER + "/business/web/course/find/"+ _this.id).then((response)=>{
+          let resp = response.data;
+          if (resp.success){
+            _this.course = resp.content;
+            _this.teacher = resp.content.teacher;
+            _this.chapters = resp.content.chapters;
+            _this.sections = resp.content.sections;
+          }
+        })
+      }
     }
   }
 </script>
 
 <style>
+   .course-head {
+   }
+   .course-head h1 {
+     font-size: 2rem;
+     margin-bottom: 1.5rem;
+   }
+   .course-head-item span {
+     margin-right: 1rem;
+   }
+   .course-head-desc {
+     font-size: 1rem;
+     color: #555
+   }
+
+   .course-head a {
+   }
+
+   .course-head-price {
+     font-size: 2rem;
+   }
+
+   /*字体大小自适应  屏幕变小就会下面的样式*/
+   @media (max-width: 700px) {
+     .course-head h1 {
+       font-size: 1.5rem;
+     }
+   }
 
 </style>
